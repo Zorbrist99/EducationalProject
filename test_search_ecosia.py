2fd24b5bd6dc1d08cb0f5235b7d672ef2ccd4ef9
U_M2ACD <sergey456859@yandex.ru>
2025-05-13 16:16:49 +0300

    Добавил себе TODO что можно сделать для решения проблем

--- test_search_ecosia.py
+++ test_search_ecosia.py
@@ -1,3 +1,4 @@
+import time
 import pytest
 from selene import browser, have, be
 
@@ -12,21 +13,24 @@ def open_browser():
 
 
 # TODO: фикстура фактически не работает. Необходимс подумать, как очищать после выполнения теста. По отдельности тесты работают
+# TODO: Добавить удаление через кнопку крестика
+# TODO: Посмотреть конфиг, который не будет ждать рендеринга страницы, а достаточно будет только дома
 @pytest.fixture(scope="function")
 def clear_search_input():
-    browser.element('[data-test-id="search-form-input"]').should(be.visible).clear()
+    browser.element('[name="q"]').should(be.visible).clear()
     yield
     # browser.element('[data-test-id="search-form-input"]').clear()
 
 
 def test_ecosia(open_browser):
     browser.element('[name="q"]').type('yashaka/selene').press_enter()
-    browser.element('[data-test-id="result-title"]').should(
+    browser.element('[id="main"]').should(
         have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
-    browser.element('[data-test-id="search-form-input"]').should(be.visible).clear()
+    time.sleep(1)
+    browser.element('[name="q"]').clear()
 
 
-def test_negative_ecosia():
+def test_negative_ecosia(open_browser, clear_search_input):
     browser.element('[name="q"]').type('234jkl32jknkflrnk23').press_enter()
     browser.element('[data-test-id="message-tips-message"]').should(
         have.text('Unfortunately we didn’t find any results for “' + '234jkl32jknkflrnk23' + '”'))

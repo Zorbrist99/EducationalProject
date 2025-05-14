3b5531e0b99c52ca9eb9d2c07435a0669d1dd7bf
U_M2ACD <sergey456859@yandex.ru>
2025-05-14 12:21:32 +0300

    Добавил функцию, которая дает возможность не ждать полной загрузки страницы. Исправил проблему с очищением строки, теперь все работает через фикстуры

--- test_search_ecosia.py
+++ test_search_ecosia.py
@@ -1,33 +1,42 @@
-import time
 import pytest
 from selene import browser, have, be
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+"""
+Варианты: 'normal', 'eager', 'none'
+'normal' — дождаться полной загрузки (по умолчанию)
+
+'eager' — дождаться DOMContentLoaded
+
+'none' — вообще не ждать полной загрузки (полный контроль, может быть быстрее, но нужно следить вручную)
+"""
+def create_driver_with_page_load_strategy():
+    options = Options()
+    options.page_load_strategy = 'eager'
+    return webdriver.Chrome(options=options)
 
 
 @pytest.fixture(scope="session")
 def open_browser():
-    # browser.config.hold_driver_at_exit = True
+    #browser.config.hold_driver_at_exit = True
+    browser.config.driver = create_driver_with_page_load_strategy()
     browser.open('https://www.ecosia.org/')
     browser.element('[data-test-id="cookie-notice-accept"]').click()
     # browser.driver.set_window_size(560, 300)
     yield
 
-
-# TODO: фикстура фактически не работает. Необходимс подумать, как очищать после выполнения теста. По отдельности тесты работают
-# TODO: Добавить удаление через кнопку крестика
-# TODO: Посмотреть конфиг, который не будет ждать рендеринга страницы, а достаточно будет только дома
 @pytest.fixture(scope="function")
 def clear_search_input():
-    browser.element('[name="q"]').should(be.visible).clear()
+    browser.element('[name="q"]').click()
+    browser.element('[data-test-id="button-icon"]').click()
     yield
-    # browser.element('[data-test-id="search-form-input"]').clear()
 
 
-def test_ecosia(open_browser):
+def test_ecosia(open_browser, clear_search_input):
     browser.element('[name="q"]').type('yashaka/selene').press_enter()
     browser.element('[id="main"]').should(
         have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
-    time.sleep(1)
-    browser.element('[name="q"]').clear()
 
 
 def test_negative_ecosia(open_browser, clear_search_input):

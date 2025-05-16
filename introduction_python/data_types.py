b3c87349c1e49736e2913ea448e1de4600692395
U_M2ACD <sergey456859@yandex.ru>
2025-05-16 12:00:42 +0300

    Потренировал работу с разными типами данных

---
+++ introduction_python/data_types.py
@@ -0,0 +1,65 @@
+"""
+Создай переменные:
+
+age — целое число
+
+height — дробное
+
+name — строка
+
+is_student — булево значение
+
+Выведи их и их типы.
+"""
+from selene.core.query import value
+
+age = 10
+print(type(age))
+print(str(age))
+print(f'Значение {age}, имеет тип {type(age)}')
+print('-------')
+
+height = 1.5
+print(type(height))
+print(height)
+print(f'Значение {height}, имеет тип {type(height)}')
+print('-------')
+
+name = 'Hi'
+print(type(name))
+print(name)
+print(f'Значение {name}, имеет тип {type(name)}')
+print('-------')
+
+is_student = True
+print(type(is_student))
+print(is_student)
+print(f'Значение {is_student}, имеет тип {type(is_student)}')
+print('-------')
+
+"""
+Напиши скрипт, который:
+
+Запрашивает у пользователя имя и возраст
+
+Выводит сообщение вида:
+"Привет, Катя! Тебе 23 года."
+"""
+
+# print('Напиши свое имя')
+# name = input()
+# print('Напиши свой возраст' )
+# value = input()
+# print(f'Привет, {name}! Тебе {value} года.')
+# print('-------')
+
+"""
+Создай переменные x = 5 и y = 2.0.
+Сложи их, умножь и выведи результат и тип каждого результата.
+"""
+x = 5
+y = 2.0
+sum= x + y
+multiplication = x * y
+print(f'Сумма выражения равна: {sum}, тип выражения: {type(sum)}')
+print(f'Результат умножения равен: {multiplication}, тип выражения: {type(multiplication)}')

bed596e04f7b0db5753fbe2dc94382aa9a4b50e1
U_M2ACD <sergey456859@yandex.ru>
2025-05-16 12:12:25 +0300

    Видоизменил команду input и команду print

---
+++ introduction_python/data_entry.py
@@ -0,0 +1,4 @@
+name = input("Напиши свое имя: ")
+value = int(input('Напиши свой возраст: '))
+print(f'Привет, {name}! Тебе {value} года.')
+print(f'Формат значения {value}, является {type(value)} ')

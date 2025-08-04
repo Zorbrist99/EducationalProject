a70c7bf5d7aa3f35c3c3725b0ce0b612c8ba8da5
U_M2ACD <sergey456859@yandex.ru>
2025-08-04 09:36:22 +0300

    Описал команды работы с файлом через модуль shutil. Начал описание работы с WebDriver selenium

--- tests/word_with_file/script_os.py
+++ tests/word_with_file/script_os.py
@@ -16,11 +16,43 @@ print(CURRENT_DIR)
 """
 os.path.join - склеивает путь до папки с тем, что мы укажем после запятой 
 """
-TMP_DIR = os.path.join(CURRENT_DIR, "[SuperSliv.biz] Задание.txt")
+TMP_DIR = os.path.join(CURRENT_DIR, "tmp")
 print(TMP_DIR)
 
-##TODO:дописать команды по проверки наличия файла.
-# if not os.path.exists("tmp2"):
-#     os.mkdir("tmp2")
-#
-# shutil.rmtree(os.path.join(CURRENT_DIR, "tmp2"))
+
+"""
+os.path.exists() - существует ли что угодно по пути (файл/папка)
+
+os.path.isfile() - существует ли именно файл
+
+os.path.isdir() - существует ли именно папка
+
+os.mkdir - создает новую папку в текущей директории. mkdir(make directory)
+"""
+if not os.path.exists("tmp2"):
+    os.mkdir("tmp2")
+
+"""
+shutil — модуль для операций с файлами и папками.
+
+rmtree — remove tree = удалить папку со всем содержимым.
+
+os.path.join(CURRENT_DIR, "tmp2") — создаёт путь до папки tmp2, которая находится в директории CURRENT_DIR.
+
+shutil.copy(src, dst)	Копирует файл (без метаинформации)
+
+shutil.copy2(src, dst)	Копирует файл с сохранением даты/прав
+
+shutil.copytree(src, dst)	Копирует папку и всё содержимое
+
+shutil.move(src, dst)	Перемещает файл или папку
+
+shutil.rmtree(path)	Полностью удаляет папку
+
+shutil.make_archive(base, fmt, dir)	Создаёт архив (zip/tar) из папки
+
+shutil.unpack_archive(file, to_dir)	Распаковывает архив
+
+shutil.disk_usage(path)	Получает объём, занято, свободно на диске
+"""
+shutil.rmtree(os.path.join(CURRENT_DIR, "tmp2"))
---
+++ tests/word_with_file/test_download_file.py
@@ -0,0 +1,26 @@
+from selene.support.shared import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+from tests.word_with_file.script_os import TMP_DIR
+
+"""
+Все команды с webDriver это настройка браузера под себя. 
+Это как будто ты говоришь:
+"Открой браузер, настрой его вот так, и отдай мне управление."
+"""
+"""
+options = webdriver.ChromeOptions()
+Создаёшь объект настроек браузера. Это как список параметров, который ты дашь браузеру при запуске.
+"""
+options = webdriver.ChromeOptions()
+
+
+prefs = {
+    "download.default_directory": TMP_DIR,
+    "download.prompt_for_download": False
+}
+options.add_experimental_option("prefs", prefs)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+browser.config.driver = driver

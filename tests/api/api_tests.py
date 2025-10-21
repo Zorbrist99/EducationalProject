719efe502dbfef47655775b844b3329c90eef7c7
U_M2ACD <sergey456859@yandex.ru>
2025-10-21 17:53:16 +0300

    .

--- tests/api/api_tests.py
+++ tests/api/api_tests.py
@@ -2,13 +2,16 @@ import json
 import requests
 import pytest
 
+
 def json_print(data):
     print(json.dumps(data, indent=4, ensure_ascii=False))
 
-#TODO:сделать валидацию ответа от api, при помощи pydantica.
+
+# TODO:сделать валидацию ответа от api, при помощи pydantica.
 def test_get_single_user():
     res = requests.get("https://reqres.in/api/users/2")
-    json_print(res.json())
-
-    assert res.status_code == 200
-
+    """
+    Метод, который проверяет был ли HTTP запрос успешным и автоматически выбрасывает исключение
+    если получен код ошибки. 200-299 - Успех. 400-599 - выбрасывает исключение HTTPError
+    """
+    res.raise_for_status()

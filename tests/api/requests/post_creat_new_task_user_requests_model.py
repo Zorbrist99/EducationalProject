d9976a1c9cd16fe649edefdb2f5b878e16483eea
U_M2ACD <sergey456859@yandex.ru>
2025-10-22 15:26:42 +0300

    Описал модель создания задачи пользователя.

---
+++ tests/api/requests/post_creat_new_task_user_requests_model.py
@@ -0,0 +1,36 @@
+"""
+{
+    "text": "Update Habitica API Documentation - Tasks",
+    "type": "todo",
+    "alias": "hab-api-tasks",
+    "notes": "Update the tasks api on GitHub",
+    "tags": [
+        "ed427623-9a69-4aac-9852-13deb9c190c3"
+    ],
+    "checklist": [
+        {
+            "text": "read wiki",
+            "completed": true
+        },
+        {
+            "text": "write code"
+        }
+    ],
+    "priority": 2
+}
+"""
+from typing import Optional, List
+
+from pydantic import BaseModel, Field
+
+
+class PostCreatNewTaskUserRequest(BaseModel):
+    text: str
+    type: str = Field(..., pattern='^(habit|daily|todo|reward)$')
+    "Таким образом можно указать варианты, которые могут вернуться, иначе ошибка"
+    frequency: str = Field(..., pattern="^(daily|weekly|monthly|yearly)$")
+    "Таким образом можно указать, что поле не обязательное"
+    alias: Optional[str] = None
+    notes: Optional[str] = None
+    daysOfMonth: List[int]
+    weeksOfMonth: List[int]

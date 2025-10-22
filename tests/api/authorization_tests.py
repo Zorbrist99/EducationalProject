643543d4d1c429114e08f702831e5460ca5e98ad
U_M2ACD <sergey456859@yandex.ru>
2025-10-22 14:50:31 +0300

    Написал api тесты на авторизацию в лк. Вынес общие фикстуры в отдельный файл conftest

--- tests/api/authorization_tests.py
+++ tests/api/authorization_tests.py
@@ -1,14 +1,22 @@
+import pytest
 
 import requests
 from tests.api.requests.post_login_in_lk_requests_model import LoginRequest
 from tests.api.responses.post_login_in_lk_response_model import PostLoginInLkResponse
 
 
-#TODO: нужно вынести работу с кредами и самим запросом из теста
-def test_login_in_lk_habitica():
-    request_data = LoginRequest(password='QAguru2024', username='beryozkinkonstantin@yandex.ru')
+@pytest.fixture
+def credits_for_authorization_incorrect():
+    return LoginRequest(
+        password='QAguru3333',
+        username='beryozkinkonstantin@yandex.ru'
+    )
+
+
+def test_login_in_lk_habitica(credits_for_authorization, api_url):
     "Метод model_dump возвращает словарь, который передается в метод post"
-    res = requests.post("https://habitica.com/api/v4/user/auth/local/login", json=request_data.model_dump())
+    res = requests.post(api_url["login"], json=credits_for_authorization.model_dump())
+
     """
         Метод, который проверяет был ли HTTP запрос успешным и автоматически выбрасывает исключение
         если получен код ошибки. 200-299 - Успех. 400-599 - выбрасывает исключение HTTPError
@@ -17,6 +25,16 @@ def test_login_in_lk_habitica():
 
     validate_response = PostLoginInLkResponse(**res.json())
 
-    print(validate_response)
     assert validate_response.success == True
     assert validate_response.data.username == 'Sergey_01'
+    assert validate_response.data.newUser == False
+
+
+def test_auth_bad_request(api_url):
+    res = requests.post(api_url["login"])
+    assert res.status_code == 400
+
+
+def test_auth_unauthorized(api_url, credits_for_authorization_incorrect):
+    res = requests.post(api_url["login"], json=credits_for_authorization_incorrect.model_dump())
+    assert res.status_code == 401
---
+++ tests/api/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+
+from tests.api.requests.post_login_in_lk_requests_model import LoginRequest
+
+
+@pytest.fixture
+def base_url():
+    return "https://habitica.com/api/v4/"
+
+
+@pytest.fixture()
+def api_url(base_url):
+    return {
+        "login": f'{base_url}user/auth/local/login'
+    }
+
+
+@pytest.fixture
+def credits_for_authorization():
+    return LoginRequest(
+        password='QAguru2024',
+        username='beryozkinkonstantin@yandex.ru'
+    )

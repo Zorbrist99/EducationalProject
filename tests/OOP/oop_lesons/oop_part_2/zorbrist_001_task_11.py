8a4afdd63d9877d8ff3eb1c635bf7f26ac15d037
U_M2ACD <sergey456859@yandex.ru>
2026-03-20 09:52:22 +0300

    Вариант реализации через метод setdefault и sum

--- tests/OOP/oop_lesons/oop_part_2/zorbrist_001_task_11.py
+++ tests/OOP/oop_lesons/oop_part_2/zorbrist_001_task_11.py
@@ -3,15 +3,18 @@ class ShoppingCart:
         self.dict_product = {}
 
     def add_item(self, name: str, price: float):
-        self.dict_product[name] = price
+        # self.dict_product[name] = price
+        "Проверим есть ли такой ключ, если нет, то создадим его. Если есть ничего не будем делать"
+        self.dict_product.setdefault(name, price)
 
     def total(self):
-        sm = 0
-        for v in self.dict_product.values():
-            sm += v
-        return sm
+        # sm = 0
+        # for v in self.dict_product.values():
+        #     sm += v
+        # return sm
+        return sum(self.dict_product.values())
 
 cart = ShoppingCart()
 cart.add_item("Apple", 3.0)
 cart.add_item("Milk", 2.5)
-print(cart.total()) # 5.5
+print(cart.total())  # 5.5
